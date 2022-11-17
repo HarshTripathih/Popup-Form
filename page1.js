4fa4a502a2b9ab3c3879faee6fa4df73ccdfe78c
HarshTripathih <harshtripathih321@gmail.com>
2022-11-17 15:37:41 +0530

    Popup Form

---
+++ page1.js
@@ -0,0 +1,8 @@
+var popupcontainer;
+
+popupcontainer = document.querySelector('.container');
+
+popupcontainer.addEventListener('click', () => {
+    popup - form.classList.add('hide');
+    container.classList.add('hide');
+});
